5b0770b14018d60db1c05292d32038f846000aee
lahmidi <dvabderrahman@gmail.com>
2024-11-26 14:11:48 +0100

    feat:add the players

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const overlay = document.getElementById("overlay");
+const chose_player = document.getElementById("chose_player");
+
+
+chose_player.addEventListener("click", function(){
+    overlay.classList.remove("hidden");    
+    overlay.classList.add("flex");
+})
+
