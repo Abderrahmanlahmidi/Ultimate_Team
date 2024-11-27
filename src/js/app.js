f64e815b442bfaaa802dcb06f52dfc1d16e63f38
lahmidi <dvabderrahman@gmail.com>
2024-11-27 15:59:13 +0100

    feat:create the form validation

--- src/js/app.js
+++ src/js/app.js
@@ -1,43 +1,48 @@
 const overlay = document.getElementById("overlay");
 const chose_player = document.getElementById("chose_player");
 const cancer_button = document.getElementById("cancer_button");
-
-chose_player.addEventListener("click", function(){
-    overlay.classList.remove("hidden");    
-    overlay.classList.add("flex");
-})
-
-cancer_button.addEventListener("click", function(){
-    overlay.classList.remove("flex");
-    overlay.classList.add("hidden");
-})
+let store_data = []
 
 
+chose_player.addEventListener("click", function () {
+  overlay.classList.remove("hidden");
+  overlay.classList.add("flex");
+});
 
+cancer_button.addEventListener("click", function () {
+  overlay.classList.remove("flex");
+  overlay.classList.add("hidden");
+});
 
 const loadPlayersData = async () => {
-   
-    try{
+  try {
+    const res = await fetch("../data/data.json");
+    let data = await res.json();
 
-        const res = await fetch("../data/data.json")
-        let data = await res.json();
-        addData(data.players)
-       
+    data.players.forEach(element => {
+      store_data.push(element);
+    });
 
-    }catch (err){
-        console.error("Error loading data", err);
-    }
+    addData(store_data);
 
-    
-}
+  } catch (err) {
+    console.error("Error loading data", err);
+  }
+};
 
 loadPlayersData();
 
 
-const addData = async (cards_data) => {
+console.log(store_data)
 
-    const displayDataCard = cards_data.map(elements => {
-        return`
+
+
+
+const addData = async (store_data) => {
+
+  const displayDataCard = store_data.map((elements) => {
+
+      return `
            <div
             draggable="true"
             class="card relative w-[95px] h-auto bg-cover bg-center bg-no-repeat p-[1.2rem_0] z-2 transition ease-in duration-200"
@@ -115,47 +120,61 @@ const addData = async (cards_data) => {
             </div>
             </div>
             </div>
-        ` 
-
-    }).join(' ');
-   
-
-    players_cards.innerHTML = displayDataCard;
-    const cards = document.querySelectorAll(".card");
-    drag_drop(cards);
+        `;
+    }).join(" ");
     
-}
+
+  players_cards.innerHTML = displayDataCard;
+  const cards = document.querySelectorAll(".card");
+  drag_drop(cards);
+};
+
+
 
 
 const players_area = document.querySelectorAll(".player_area");
 const players_cards = document.getElementById("players_cards");
 
 const drag_drop = (cards) => {
+    
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("dragstart", (e) => {
+      let selected = e.currentTarget;
 
-    for (let i = 0; i < cards.length; i++) {
-        
-        cards[i].addEventListener("dragstart", (e) => {
-            let selected = e.target;
+      players_cards.addEventListener("dragover", function (e) {
+        e.preventDefault();
 
-         players_cards.addEventListener("dragover", function(e){
-            e.preventDefault();
+        overlay.classList.add("hidden");
+      });
 
-            overlay.classList.add("hidden");
+      players_area[i].addEventListener("drop", function (e) {
+        players_area[i].appendChild(selected);
+        selected = null;
+      });
+    });
+  }
+};
 
-         });
-
-         players_area[i].addEventListener("drop", function(e){
-            players_area[i].appendChild(selected);
-            selected = null;
-
-         })
+const add_player = document.getElementById("add_player");
+const add_overlay = document.getElementById("add_overlay");
+const cancer_add_button = document.getElementById("cancer_add_button");
 
 
-        })
-        
-    }
+add_player.addEventListener("click", function(){
+   add_overlay.classList.add("flex");
+   add_overlay.classList.remove("hidden");
+});
 
-}
+cancer_add_button.addEventListener("click", function(){
+    add_overlay.classList.add("hidden");
+    add_overlay.classList.remove("flex");
+});
+
+
+
+
+
+
 
 
 
